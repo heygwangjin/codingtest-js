0970d3fcb0621ba6dae09f8b7a5f52f150203f20
heygwangjin <heygwangjin@gmail.com>
2024-11-25 18:14:46 +0900

    Solve 2739

---
+++ solved.ac/class-01/2739.js
@@ -0,0 +1,24 @@
+function solution(n) {
+  let result = '';
+  for (let i = 1; i <= 9; i++) {
+    result += `${n} * ${i} = ${n * i}\n`;
+  }
+  return result;
+}
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input;
+
+rl.on('line', (line) => {
+  input = line;
+  input = Number(line);
+  rl.close();
+}).on('close', () => {
+  console.log(solution(input));
+  process.exit();
+});
