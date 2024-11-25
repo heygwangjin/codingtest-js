85af6a9fa6aa8868f2236c7f97b70dd921fa736c
heygwangjin <heygwangjin@gmail.com>
2024-11-25 18:50:47 +0900

    Solve 11654

---
+++ solved.ac/class-01/11654.js
@@ -0,0 +1,19 @@
+function solution(char) {
+  return char.charCodeAt();
+}
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input;
+
+rl.on('line', (line) => {
+  input = line;
+  rl.close();
+}).on('close', () => {
+  console.log(solution(input));
+  process.exit();
+});
