2b7609bfe23ac887b68f99d4cee3fd706b7d7276
heygwangjin <heygwangjin@gmail.com>
2024-11-25 17:51:10 +0900

    Solve 2475

---
+++ solved.ac/class-01/2475.js
@@ -0,0 +1,26 @@
+function solution(arr) {
+  let result = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    result += Math.pow(arr[i], 2);
+  }
+
+  return result % 10;
+}
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input;
+
+rl.on('line', (line) => {
+  input = line;
+  rl.close();
+}).on('close', () => {
+  const arr = input.split(' ').map((el) => Number(el));
+  console.log(solution(arr));
+  process.exit();
+});
