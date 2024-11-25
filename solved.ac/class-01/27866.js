09b88d3aca7f7ed6b4012a8cdd3914967e920d2a
heygwangjin <heygwangjin@gmail.com>
2024-11-25 18:53:34 +0900

    Solve 27866

---
+++ solved.ac/class-01/27866.js
@@ -0,0 +1,18 @@
+function solution(arr) {
+  const [str, i] = arr;
+  return str[i - 1];
+}
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const arr = [];
+rl.on('line', (line) => {
+  arr.push(line.trim());
+}).on('close', () => {
+  console.log(solution(arr));
+  process.exit();
+});
