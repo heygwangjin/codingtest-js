e47a1addb7619717b37c9c7da43544933cf18119
heygwangjin <heygwangjin@gmail.com>
2024-11-29 20:31:04 +0900

    Update BST

--- data-structure/binary-search-tree.js
+++ data-structure/binary-search-tree.js
@@ -14,23 +14,24 @@ class BST {
   insert(key) {
     if (!this.root) {
       this.root = new Node(key);
-    } else {
-      let curr = this.root;
-      while (true) {
-        if (key < curr.val) {
-          if (curr.left) {
-            curr = curr.left;
-          } else {
-            curr.left = new Node(key);
-            break;
-          }
+      return;
+    }
+
+    let curr = this.root;
+    while (true) {
+      if (key < curr.val) {
+        if (curr.left) {
+          curr = curr.left;
         } else {
-          if (curr.right) {
-            curr = curr.right;
-          } else {
-            curr.right = new Node(key);
-            break;
-          }
+          curr.left = new Node(key);
+          break;
+        }
+      } else {
+        if (curr.right) {
+          curr = curr.right;
+        } else {
+          curr.right = new Node(key);
+          break;
         }
       }
     }
